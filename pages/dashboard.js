8d8c288452fd198d6d9ec88642c399ccfa647e42
viniciusdev772 <viniciusdev37@gmail.com>
2024-02-27 23:22:33 -0300

    Atualizações na lista de itens do painel de controle

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -251,7 +251,7 @@ export default function Dashboard() {
           ) : null}
           <ItemList
             espacoDisponivel={espacoDisponivel}
-            items={items}
+            items={filteredItems}
             onItemSelected={openActionsModal}
           />
         </div>
